8be89a1b1ef1b44d093e22da1024a0d3aa5754d2
not-sdub <stanwilde99@outlook.com>
2025-08-08 22:11:59 +0100

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'screens/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'I Spy App',
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
